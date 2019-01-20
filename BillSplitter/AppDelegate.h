d788b5e74a983a0bda64774ec1414c16c918f135
Matthew Chan <mat_t4@hotmail.com>
2019-01-19 21:45:34 -0800

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Matthew Chan on 2019-01-19.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Matthew Chan on 2019-01-19.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
